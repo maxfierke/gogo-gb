422ef639c25ae21f48bfc8ad110b1c5f875a5321
Max Fierke <max@maxfierke.com>
2025-06-08 22:30:32 -0500

    Implement booting into CGB mode with a CGB bootrom

--- cmd/run.go
+++ cmd/run.go
@@ -23,6 +23,7 @@ type RunCmdOptions struct {
 	cartSavePath string
 	debugger     string
 	headless     bool
+	model        string
 	serialPort   string
 	skipBootRom  bool
 }
@@ -64,6 +65,7 @@ func init() {
 	_ = runCmd.MarkFlagFilename("save", ".sav")
 
 	runCmd.Flags().StringVarP(&runCmdOptions.debugger, "debugger", "d", "", "Specify debugger to use (\"gameboy-doctor\", \"interactive\")")
+	runCmd.Flags().StringVarP(&runCmdOptions.model, "model", "m", "auto", "Specify model to use (\"auto\", \"dmg\", \"cgb\")")
 	runCmd.Flags().StringVarP(&runCmdOptions.serialPort, "serial-port", "p", "", "Path to serial port IO (could be a file, UNIX socket, etc.)")
 	runCmd.Flags().BoolVar(&runCmdOptions.skipBootRom, "skip-bootrom", false, "Skip loading a boot ROM")
 	runCmd.Flags().BoolVar(&runCmdOptions.headless, "headless", false, "Launch without UI")
@@ -74,6 +76,19 @@ var DEFAULT_BOOT_ROM_PATHS = []string{
 	"dmg_bios.bin",
 	"mgb_bios.bin",
 	"dmg0_bios.bin",
+	"gb_boot.bin",
+	"dmg_boot.bin",
+	"mgb_boot.bin",
+	"dmg0_boot.bin",
+}
+
+var DEFAULT_CGB_BOOT_ROM_PATHS = []string{
+	"cgb_bios.bin",
+	"cgb0_bios.bin",
+	"gbc_bios.bin",
+	"gbc_boot.bin",
+	"cgb_boot.bin",
+	"cgb0_boot.bin",
 }
 
 func getCartSaveFilePath(options *RunCmdOptions) string {
@@ -129,10 +144,10 @@ func initHost(logger *log.Logger, options *RunCmdOptions) (host.Host, error) {
 	return hostDevice, nil
 }
 
-func initDMG(logger *log.Logger, options *RunCmdOptions) (*hardware.DMG, error) {
+func initConsole(logger *log.Logger, options *RunCmdOptions) (hardware.Console, error) {
 	debugger, err := debug.NewDebugger(options.debugger)
 	if err != nil {
-		return nil, fmt.Errorf("unable to initialize Debugger: %w", err)
+		return nil, fmt.Errorf("unable to initialize debugger: %w", err)
 	}
 
 	opts := []hardware.ConsoleOption{
@@ -154,12 +169,35 @@ func initDMG(logger *log.Logger, options *RunCmdOptions) (*hardware.DMG, error) 
 		}
 	}
 
-	dmg, err := hardware.NewDMG(opts...)
-	if err != nil {
-		return nil, fmt.Errorf("unable to initialize DMG: %w", err)
+	var model hardware.ConsoleModel
+	switch options.model {
+	case "auto":
+		ext := filepath.Ext(options.cartPath)
+		switch ext {
+		case ".gb":
+			model = hardware.ConsoleModelDMG
+		case ".gbc":
+			model = hardware.ConsoleModelCGB
+		default:
+			return nil, errors.New("unable to auto-detect model. Please specify with --model/-m")
+		}
+	case "dmg":
+		model = hardware.ConsoleModelDMG
+	case "cgb":
+		model = hardware.ConsoleModelCGB
+	default:
+		return nil, fmt.Errorf("unrecognized model: %s", options.model)
 	}
 
-	return dmg, nil
+	console, err := hardware.NewConsole(
+		model,
+		opts...,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return console, nil
 }
 
 func loadBootROM(logger *log.Logger, options *RunCmdOptions) (*os.File, error) {
@@ -169,7 +207,12 @@ func loadBootROM(logger *log.Logger, options *RunCmdOptions) (*os.File, error) {
 	var err error
 
 	if bootRomPath == "" {
-		for _, romPath := range DEFAULT_BOOT_ROM_PATHS {
+		lookupPaths := DEFAULT_BOOT_ROM_PATHS
+		if options.model == "cgb" {
+			lookupPaths = DEFAULT_CGB_BOOT_ROM_PATHS
+		}
+
+		for _, romPath := range lookupPaths {
 			if bootRomFile, err = os.Open(romPath); err != nil && !errors.Is(err, os.ErrNotExist) {
 				return nil, err
 			} else if bootRomFile != nil {
@@ -192,7 +235,7 @@ func loadBootROM(logger *log.Logger, options *RunCmdOptions) (*os.File, error) {
 	return bootRomFile, nil
 }
 
-func loadCart(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions) error {
+func loadCart(console hardware.Console, logger *log.Logger, options *RunCmdOptions) error {
 	if options.cartPath == "" {
 		return nil
 	}
@@ -203,7 +246,7 @@ func loadCart(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions) err
 	}
 	defer cartFile.Close()
 
-	err = dmg.LoadCartridge(cartFile)
+	err = console.LoadCartridge(cartFile)
 	if errors.Is(err, cart.ErrChecksum) {
 		logger.Printf("WARN: Cartridge header does not match expected checksum. Continuing, but subsequent operations may fail")
 	} else if err != nil {
@@ -213,7 +256,7 @@ func loadCart(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions) err
 	return nil
 }
 
-func loadCartSave(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions) error {
+func loadCartSave(console hardware.Console, logger *log.Logger, options *RunCmdOptions) error {
 	cartSaveFilePath := getCartSaveFilePath(options)
 
 	cartSaveFile, err := os.Open(cartSaveFilePath)
@@ -224,7 +267,7 @@ func loadCartSave(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions)
 	if cartSaveFile != nil {
 		defer cartSaveFile.Close()
 
-		err = dmg.LoadSave(cartSaveFile)
+		err = console.LoadSave(cartSaveFile)
 		if err != nil {
 			switch {
 			case errors.Is(err, mbc.ErrMBC3BadClockBattery):
@@ -240,7 +283,7 @@ func loadCartSave(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions)
 	return nil
 }
 
-func saveCart(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions) error {
+func saveCart(console hardware.Console, logger *log.Logger, options *RunCmdOptions) error {
 	cartSaveFilePath := getCartSaveFilePath(options)
 
 	cartSaveFile, err := os.OpenFile(cartSaveFilePath, os.O_RDWR|os.O_CREATE, 0o644)
@@ -249,7 +292,7 @@ func saveCart(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions) err
 	}
 	defer cartSaveFile.Close()
 
-	err = dmg.Save(cartSaveFile)
+	err = console.Save(cartSaveFile)
 	if err != nil {
 		return fmt.Errorf("unable to write cartridge save file: %w", err)
 	}
@@ -260,36 +303,36 @@ func saveCart(dmg *hardware.DMG, logger *log.Logger, options *RunCmdOptions) err
 }
 
 func runCart(logger *log.Logger, options *RunCmdOptions) error {
-	hostDevice, err := initHost(logger, options)
+	consoleHost, err := initHost(logger, options)
 	if err != nil {
 		return fmt.Errorf("unable to initialize host device: %w", err)
 	}
 
-	dmg, err := initDMG(logger, options)
+	console, err := initConsole(logger, options)
 	if err != nil {
 		return fmt.Errorf("initializing DMG: %w", err)
 	}
 
-	err = loadCart(dmg, logger, options)
+	err = loadCart(console, logger, options)
 	if err != nil {
 		return fmt.Errorf("loading cartridge: %w", err)
 	}
 
-	if dmg.CartridgeHeader().SupportsSaving() {
-		err := loadCartSave(dmg, logger, options)
+	if console.CartridgeHeader().SupportsSaving() {
+		err := loadCartSave(console, logger, options)
 		if err != nil {
 			return fmt.Errorf("loading cartridge save: %w", err)
 		}
 
 		defer func() {
-			err := saveCart(dmg, logger, options)
+			err := saveCart(console, logger, options)
 			if err != nil {
 				logger.Printf("WARN: Error occurred while saving: %s", err.Error())
 			}
 		}()
 	}
 
-	err = hostDevice.Run(dmg)
+	err = consoleHost.Run(console)
 	if err != nil {
 		return fmt.Errorf("running emulation: %w", err)
 	}
---
+++ hardware/cgb.go
@@ -0,0 +1,227 @@
+package hardware
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"io"
+
+	"github.com/maxfierke/gogo-gb/cart"
+	"github.com/maxfierke/gogo-gb/cpu"
+	"github.com/maxfierke/gogo-gb/debug"
+	"github.com/maxfierke/gogo-gb/devices"
+	"github.com/maxfierke/gogo-gb/mem"
+)
+
+const (
+	cgbCyclesPerFrame uint = dmgCyclesPerFrame * 2
+)
+
+type CGB struct {
+	// Components
+	cpu       *cpu.CPU
+	mmu       *mem.MMU
+	cartridge *cart.Cartridge
+	dma       *devices.DMA
+	ic        *devices.InterruptController
+	joypad    *devices.Joypad
+	ppu       *devices.PPU
+	serial    *devices.SerialPort
+	timer     *devices.Timer
+
+	// Non-components
+	debugger        debug.Debugger
+	debuggerHandler mem.MemHandlerHandle
+}
+
+var _ Console = (*CGB)(nil)
+
+func NewCGB(opts ...ConsoleOption) (*CGB, error) {
+	cgbCpu, err := cpu.NewCPU()
+	if err != nil {
+		return nil, fmt.Errorf("constructing CPU: %w", err)
+	}
+
+	// Enable CGB CPU Features
+	cgbCpu.EnableFeature(cpu.FeatureDoubleSpeed)
+
+	ram := make([]byte, DMG_RAM_SIZE)
+	mmu := mem.NewMMU(ram)
+	echo := mem.NewEchoRegion()
+	unmapped := mem.NewUnmappedRegion()
+
+	ic := devices.NewInterruptController()
+
+	cgb := &CGB{
+		cpu:       cgbCpu,
+		mmu:       mmu,
+		cartridge: cart.NewCartridge(),
+		debugger:  debug.NewNullDebugger(),
+		dma:       devices.NewDMA(),
+		ic:        ic,
+		joypad:    devices.NewJoypad(ic),
+		ppu:       devices.NewPPU(ic),
+		serial:    devices.NewSerialPort(),
+		timer:     devices.NewTimer(),
+	}
+
+	for _, opt := range opts {
+		err = opt(cgb, mmu)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	mmu.AddHandler(mem.MemRegion{Start: 0x0000, End: 0x7FFF}, cgb.cartridge) // MBCs ROM Banks
+	mmu.AddHandler(mem.MemRegion{Start: 0xA000, End: 0xBFFF}, cgb.cartridge) // MBCs RAM Banks
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xE000, End: 0xFDFF}, echo)     // Echo RAM (mirrors WRAM)
+	mmu.AddHandler(mem.MemRegion{Start: 0xFEA0, End: 0xFEFF}, unmapped) // Nop writes, zero reads
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF00, End: 0xFF00}, cgb.joypad) // Joypad
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF01, End: 0xFF02}, cgb.serial) // Serial Port (Control & Data)
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF04, End: 0xFF07}, cgb.timer)  // Timer (not RTC)
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF40, End: 0xFF41}, cgb.ppu)    // LCD status, control registers
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF42, End: 0xFF45}, cgb.ppu)    // PPU registers
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF46, End: 0xFF46}, cgb.dma)    // DMA
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF47, End: 0xFF4B}, cgb.ppu)    // PPU registers
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF4F, End: 0xFF45}, unmapped) // VRAM Bank Select
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF51, End: 0xFF55}, unmapped) // VRAM DMA
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF56, End: 0xFF56}, unmapped) // IR Port
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF68, End: 0xFF6B}, unmapped) // BG/OBJ Palettes
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF6C, End: 0xFF6C}, unmapped) // OBJ Priority Mode
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF70, End: 0xFF70}, unmapped) // WRAM Bank Select
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF72, End: 0xFF73}, unmapped) // Unknown, should be R/W on CGB
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF74, End: 0xFF74}, unmapped) // Unknown, should be R/W on CGB
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF75, End: 0xFF75}, unmapped) // Unknown, bits 4-6 should be R/W on CGB
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF4D, End: 0xFF4D}, cgb.cpu) // CPU Speed Switch
+
+	mmu.AddHandler(mem.MemRegion{Start: 0x8000, End: 0x9FFF}, cgb.ppu) // VRAM tiles
+	mmu.AddHandler(mem.MemRegion{Start: 0xFE00, End: 0xFE9F}, cgb.ppu) // OAM
+
+	mmu.AddHandler(mem.MemRegion{Start: 0xFF0F, End: 0xFF0F}, cgb.ic) // Interrupts Requested
+	mmu.AddHandler(mem.MemRegion{Start: 0xFFFF, End: 0xFFFF}, cgb.ic) // Interrupts Enabled
+
+	return cgb, nil
+}
+
+func (cgb *CGB) AttachCable(cable devices.SerialCable) {
+	cgb.serial.AttachCable(cable)
+}
+
+func (cgb *CGB) AttachDebugger(debugger debug.Debugger) {
+	cgb.detachDebugger()
+
+	cgb.debuggerHandler = cgb.mmu.AddHandler(mem.MemRegion{Start: 0x0000, End: 0xFFFF}, debugger)
+	cgb.debugger = debugger
+}
+
+func (cgb *CGB) detachDebugger() {
+	// Remove any existing handlers
+	cgb.mmu.RemoveHandler(cgb.debuggerHandler)
+	cgb.debugger = debug.NewNullDebugger()
+}
+
+func (cgb *CGB) SetupDebugger() {
+	cgb.debugger.Setup(cgb.cpu, cgb.mmu, cgb.cartridge)
+}
+
+func (cgb *CGB) Debugger() debug.Debugger {
+	return cgb.debugger
+}
+
+func (cgb *CGB) CartridgeHeader() cart.Header {
+	if cgb.cartridge == nil {
+		return cart.Header{}
+	}
+
+	return cgb.cartridge.Header
+}
+
+func (cgb *CGB) CyclesPerFrame() uint {
+	if cgb.cpu.IsDoubleSpeed() {
+		return cgbCyclesPerFrame
+	}
+
+	return dmgCyclesPerFrame
+}
+
+func (cgb *CGB) LoadCartridge(r io.Reader) error {
+	cartReader, err := cart.NewReader(r)
+	if err != nil && !errors.Is(err, cart.ErrChecksum) {
+		return fmt.Errorf("loading cartridge: %w", err)
+	}
+
+	err = cgb.cartridge.LoadCartridge(cartReader)
+	if err != nil {
+		return fmt.Errorf("loading cartridge: %w", err)
+	}
+
+	return nil
+}
+
+func (cgb *CGB) Draw() image.Image {
+	return cgb.ppu.Draw()
+}
+
+func (cgb *CGB) LoadSave(r io.Reader) error {
+	err := cgb.cartridge.LoadSave(r)
+	if err != nil {
+		return fmt.Errorf("loading save: %w", err)
+	}
+
+	return nil
+}
+
+func (cgb *CGB) Save(w io.Writer) error {
+	err := cgb.cartridge.Save(w)
+	if err != nil {
+		return fmt.Errorf("writing save: %w", err)
+	}
+
+	return nil
+}
+
+func (cgb *CGB) ReceiveInputs(inputs devices.JoypadInputs) {
+	cgb.joypad.ReceiveInputs(inputs)
+}
+
+func (cgb *CGB) Step() (uint8, error) {
+	cgb.debugger.OnDecode(cgb.cpu, cgb.mmu)
+
+	var cycles uint8
+
+	haltedPriorToExecute := cgb.cpu.IsHalted()
+
+	cycles, err := cgb.cpu.Step(cgb.mmu)
+	if err != nil {
+		return 0, fmt.Errorf("unexpected error while executing instruction: %w", err)
+	}
+
+	// We're checking the halted state from _before_ the current instruction was
+	// executed, because we want to trigger the OnExecute for the HALT instruction
+	// itself, but not while halted, since the CPU isn't really executing during
+	// this time.
+	if !haltedPriorToExecute {
+		cgb.debugger.OnExecute(cgb.cpu, cgb.mmu)
+	}
+
+	hasInterrupt, intCycles := cgb.cpu.PollInterrupts(cgb.mmu, cgb.ic)
+	if hasInterrupt {
+		cycles += intCycles
+		cgb.debugger.OnInterrupt(cgb.cpu, cgb.mmu)
+	}
+
+	cgb.cartridge.Step(cycles)
+	cgb.dma.Step(cgb.mmu, cycles)
+	cgb.ppu.Step(cycles)
+	cgb.timer.Step(cycles, cgb.ic)
+	cgb.serial.Step(cycles, cgb.ic)
+
+	return cycles, nil
+}
